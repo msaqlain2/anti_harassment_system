b1fd686a464460f6f78e28393f2e0d02b016a44b
Muhammad Saqlain <msaqlain955@gmail.com>
2023-01-25 01:33:37 +0500

    done

--- index.php
+++ index.php
@@ -65,33 +65,6 @@
         </div>
     </div>
     <!-- Carousel End -->
-
-
-
-    <!-- About Start -->
-    <div class="container-fluid about pt-5">
-        <div class="container">
-            <div class="row gx-5">
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="d-flex h-100  pt-4">
-                        <img class="img-fluid mt-auto mx-auto" src="img/New_Project.png">
-                    </div>
-                </div>
-                <div class="col-lg-6 pb-5">
-                    <div class="mb-3 pb-2">
-                        <h6 class="text-primary text-uppercase">About Us</h6>
-                        <h1 class="display-5">Anti Harassment</h1>
-                    </div>
-                    <p class="mb-4">Harassment is an unwanted behavior, physical verbal, psychology etc that makes a reasonable person feel uncomfortable.
-"Protection against harassment for women" through this website women can file their report online instead of going to the concern department to report it. And the anti harassment cell has authority to send the report of the harassed women through the website instead of going to organization.
-Through this website we can reduce the cases by taking action against harassment and women will have platform that they can file a report sitting at their home,offices and hostels or any where through this website</p>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- About End -->
-
     
 
     <!-- Services Start -->
